7957af02cf73639c2ce350cdff12415b2e6df9d5
Adnan <adnan.goulamaly@gmail.com>
2020-11-04 16:56:18 +0400

    mise en forme de la page de connexion

--- src/components/signInForm.js
+++ src/components/signInForm.js
@@ -1,13 +1,23 @@
 import React from "react";
+import "./signInForm.css";
 
 const SignInForm = () => {
-    return(
-        <form>
-          <label for="userName">Nom d'utilisateur </label>
-          <input type="text" className="form-control" id="userName" />
-          <label for="passwordInput">Mot de passe</label>
-          <input type="password" className="form-control" id="passwordInput" />
-      </form>
+    return (
+        <div className="container" id="SignInComponent" >
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">Connexion </h5>
+                    <form>
+                        <label for="userName">Nom d'utilisateur </label>
+                        <input type="text" className="form-control" id="userName" />
+                        <label for="passwordInput">Mot de passe</label>
+                        <input type="password" className="form-control" id="passwordInput" />
+                    </form>
+                </div>
+            </div>
+        </div>
+
+
     )
 }
 
--- src/components/signUpForm.js
+++ src/components/signUpForm.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 const SignUpForm = () => {
-    return(
+    return (
         <form>
-          <label for="lastNameInput">Nom </label>
-          <input type="text" className="form-control" id="lastNameInput" />
-          <label for="emailInput">Adresse e-mail</label>
-          <input type="email" className="form-control" id="emailInput" placeholder="name@example.com" />
-      </form>
+            <label for="lastNameInput">Nom </label>
+            <input type="text" className="form-control" id="lastNameInput" />
+            <label for="emailInput">Adresse e-mail</label>
+            <input type="email" className="form-control" id="emailInput" placeholder="name@example.com" />
+        </form>
+
     )
 }
 
