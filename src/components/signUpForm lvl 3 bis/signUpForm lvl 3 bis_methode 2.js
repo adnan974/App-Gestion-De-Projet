6bf63f4ef44a3da7770ea5359ba792a252a1e1c7
Adnan <adnan.goulamaly@gmail.com>
2020-11-13 12:43:28 +0400

    module 1 lvl 3 début de documentation + correction d'un bug

--- src/components/signUpForm lvl 3 bis/signUpForm lvl 3 bis_methode 2.js
+++ src/components/signUpForm lvl 3 bis/signUpForm lvl 3 bis_methode 2.js
@@ -27,42 +27,54 @@ const SignUpForm = () => {
 
 
 
+    // TUTO : formik avec des composants
     return (
         <div className="container" id="SignInComponent" >
             <div className="card">
                 <div className="card-body">
                     <h5 className="card-title">S'inscrire </h5>
 
+                    {/* equivalent de useFormik */}
                     <Formik
                         initialValues={initialValues}
                         validationSchema={validationSchema}
                         onSubmit={onSubmit}
                     >
+                        {/* equivalent d'un <form> avec handleSubmit déjà associé */}
                         <Form>
                             <div>
-                                <label htmlFor="name">Nom</label>
-                                <Field type="text" className="form-control" id="name" name="name" />
-                                <ErrorMessage name="name" />
+
+                                <div>
+                                    <label htmlFor="name">Nom</label>
+                                    {/* Peut être (je crois) n'importe quel type de champs. par défaut, c'est un input. 
+                                       - En fonction de l'attribut  as="", on peut chosiir le type de champs voulu. On peut aussi ajouter les attributs qu'on veut.
+                                       Ex. ClassName
+                                    */}
+                                    <Field type="text" className="form-control" id="name" name="name" />
+
+                                    {/* */}
+                                    <ErrorMessage name="name" />
+                                </div>
+
+                                <div>
+                                    <label htmlFor="email">E-mail</label>
+                                    <Field type="email" className="form-control" id="email" name="email" />
+                                    <ErrorMessage name="email" />
+                                </div>
+
+                                <div>
+                                    <label htmlFor="channel">channel</label>
+                                    <Field type="text" className="form-control" id="channel" name="channel" />
+                                    <ErrorMessage name="channel" />
+                                </div>
+
+
+
+                                <button type="submit" className="btn btn-outline-primary" >valider</button>
                             </div>
-
-                            <div>
-                                <label htmlFor="email">E-mail</label>
-                                <Field type="email" className="form-control" id="email" name="email" />
-                                <ErrorMessage name="email" />
-                            </div>
-
-                            <div>
-                                <label htmlFor="channel">channel</label>
-                                <Field type="text" className="form-control" id="channel" name="channel" />
-                                <ErrorMessage name="channel" />
-                            </div>
-
-
-
-                            <button type="submit" className="btn btn-outline-primary" >valider</button>
-
+                            <Link to="/signIn">se connecter</Link>
                         </Form>
-                        <Link to="/signIn">se connecter</Link>
+                        
                     </Formik>
                 </div>
             </div>
