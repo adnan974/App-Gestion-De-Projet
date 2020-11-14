0886bd963cc8e59ba6f6eefc83e112178ecbbdc4
adn974 <56817528+adnan974@users.noreply.github.com>
2020-11-14 16:41:49 +0400

    Finnalisation du module 1

--- src/components/signInForm.js
+++ src/components/signInForm.js
@@ -1,20 +1,46 @@
 import React from "react";
 import "./signInForm.css";
 import { Link } from "react-router-dom";
+import { Formik, Form, Field } from "formik";
+import *  as Yup from "yup";
 
 const SignInForm = () => {
+
+
+    const initialValues = {
+        userName: "",
+        password: "",
+    }
+
+    const onSubmit = () => {
+        console.log("Submitted succesfully")
+    }
+
+    const validationSchema = Yup.object({
+        userName: Yup.string().required("Champs obligatoire"),
+        password: Yup.string().required("Champs obligatoire")
+
+    })
+
     return (
         <div className="container" id="SignInComponent" >
             <div className="card">
                 <div className="card-body">
                     <h5 className="card-title">Connexion </h5>
-                    <form>
-                        <label htmlFor="userName">Nom d'utilisateur </label>
-                        <input type="text" className="form-control" id="userName" />
-                        <label htmlFor="passwordInput">Mot de passe</label>
-                        <input type="password" className="form-control" id="passwordInput" />
-                        <button type="submit" className="btn btn-outline-primary" >valider</button>
-                    </form>
+                    <Formik
+                        initialValues={initialValues}
+                        onSubmit={onSubmit}
+                        validationSchema={validationSchema}
+                    >
+                        <Form>
+                            <label htmlFor="userName">Nom d'utilisateur </label>
+                            <Field name="userName" type="text" className="form-control" id="userName" />
+                            <label htmlFor="password">Mot de passe</label>
+                            <Field name="password" type="password" className="form-control" id="password" />
+
+                            <button type="submit" className="btn btn-outline-primary" >valider</button>
+                        </Form>
+                    </Formik>
                     <Link to="/signUp">s'inscrire</Link>
                 </div>
             </div>
--- src/components/signUpForm lvl 3 bis/signUpForm lvl 3 bis.js
+++ src/components/signUpForm lvl 3 bis/signUpForm lvl 3 bis.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import "./signInForm.css";
+import "./signUpForm.css";
 import * as Yup from "yup";
+import { Link } from "react-router-dom";
+
 
 
 const SignUpForm = () => {
@@ -24,7 +26,9 @@ const SignUpForm = () => {
         email: Yup.string().email("Le format du mail est invalide").required("Champs obligatoire"),
         userName: Yup.string().required("Champs obligatoire"),
         password: Yup.string().required("Champs obligatoire"),
-        confirmPassword: Yup.string().required("Champs obligatoire"),
+        passwordConfirmation: Yup.string()
+            .oneOf([Yup.ref('password'), null], 'Les mots de passes doivent être identiques').required("champs obligatoire")
+
     })
 
     return (
@@ -36,56 +40,56 @@ const SignUpForm = () => {
                         initialValues={initialValues}
                         onSubmit={onSubmit}
                         validationSchema={validationSchema}
-                        
+
                     >
+                        <div>
+                            <Form>
+                                <div className="form-block">
+                                    <label htmlFor="firstName" > Prénom </label>
+                                    <Field type="text" id="firstName" name="firstName" className="form-control" />
+                                    <ErrorMessage name="firstName"  >{msg => <small className="text-danger">{msg}</small>}</ErrorMessage>
+                                </div>
 
-                        <Form>
-                            <div>
-                                <label htmlFor="firstName" > Prénom </label>
-                                <Field type="text" id="firstName" name="firstName" className="form-control" />
-                                <ErrorMessage name="firstName"  >{msg => <small className = "text-danger">{msg}</small>}</ErrorMessage>
-                            </div>
+                                <div className="form-block">
+                                    <label htmlFor="lastName" > Nom </label>
+                                    <Field type="text" id="lastName" name="lastName" className="form-control" />
+                                    <ErrorMessage name="lastName" >{msg => <small className="text-danger">{msg}</small>}</ErrorMessage>
 
-                            <div>
-                                <label htmlFor="lastName" > Nom </label>
-                                <Field type="text" id="lastName" name="lastName" className="form-control" />
-                                <ErrorMessage name="lastName" >{msg => <small className = "text-danger">{msg}</small>}</ErrorMessage>
+                                </div>
 
-                            </div>
-                            
-                            <div>
-                                <label htmlFor="email" > E-mail </label>
-                                <Field type="email" id="email" name="email" className="form-control" />
-                                <ErrorMessage name="email"  >{msg => <small className = "text-danger">{msg}</small>}</ErrorMessage>
+                                <div className="form-block">
+                                    <label htmlFor="email" > E-mail </label>
+                                    <Field type="email" id="email" name="email" className="form-control" />
+                                    <ErrorMessage name="email"  >{msg => <small className="text-danger">{msg}</small>}</ErrorMessage>
 
-                            </div>
+                                </div>
 
-                            <div>
-                                <label htmlFor="userName" > Nom d'utilisateur </label>
-                                <Field type="text" id="userName" name="userName" className="form-control" />
-                                <ErrorMessage name="userName"  >{msg => <small className = "text-danger">{msg}</small>}</ErrorMessage>
+                                <div className="form-block">
+                                    <label htmlFor="userName" > Nom d'utilisateur </label>
+                                    <Field type="text" id="userName" name="userName" className="form-control" />
+                                    <ErrorMessage name="userName"  >{msg => <small className="text-danger">{msg}</small>}</ErrorMessage>
 
-                            </div>
-                            
-                            <div>
-                                <label htmlFor="password" > Mot de passe</label>
-                                <Field type="password" id="password" name="password" className="form-control" />
-                                <ErrorMessage name="password" >{msg => <small className = "text-danger">{msg}</small>}</ErrorMessage>
+                                </div>
 
-                            </div>
-                            
-                            <div>
-                                <label htmlFor="confirmPassword" > Confirmation mot de passe </label>
-                                <Field type="password" id="confirmPassword" name="confirmPassword" className="form-control" />
-                                <ErrorMessage name="confirmPassword"   >{msg => <small className = "text-danger">{msg}</small>}</ErrorMessage>
+                                <div className="form-block">
+                                    <label htmlFor="password" > Mot de passe</label>
+                                    <Field type="password" id="password" name="password" className="form-control" />
+                                    <ErrorMessage name="password" >{msg => <small className="text-danger">{msg}</small>}</ErrorMessage>
 
-                            </div>
-                            
-                            <button type="submit" className="btn btn-outline-primary" > Valider </button>
+                                </div>
 
-                        </Form>
+                                <div className="form-block">
+                                    <label htmlFor="passwordConfirmation" > Confirmation mot de passe </label>
+                                    <Field type="password" id="passwordConfirmation" name="passwordConfirmation" className="form-control" />
+                                    <ErrorMessage name="passwordConfirmation"   >{msg => <small className="text-danger">{msg}</small>}</ErrorMessage>
 
+                                </div>
 
+                                <button type="submit" className="btn btn-outline-primary" > Valider </button>
+
+                            </Form>
+                            <Link to="/signIn">Se connecter</Link>
+                        </div>
                     </Formik>
                 </div>
             </div>
