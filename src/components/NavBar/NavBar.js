862297095e7460c7423e8822613fc2735770e4fb
Adnan <adnan.goulamaly@gmail.com>
2020-11-19 12:35:48 +0400

    ajout d un debut de side bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,30 @@
 import React from "react";
+import { ProSidebar, Menu, MenuItem, SubMenu, SidebarHeader, SidebarContent ,SidebarFooter} from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
 
-const NavBar = () =>{
+const NavBar = () => {
 
-    return(
-        <div>
+    return (
+        <ProSidebar>
+            <SidebarHeader>
+                MENU
 
-            NAVBAR
-        </div>
+            </SidebarHeader>
+            <SidebarContent>
+                <Menu iconShape="square">
+                    <MenuItem >Tableau de Bord</MenuItem>
+                    
+                    <MenuItem> Projet </MenuItem>
+                    <MenuItem> Tâches </MenuItem>
+                </Menu>
+
+            </SidebarContent>
+            <SidebarFooter>
+                Footer
+
+            </SidebarFooter>
+
+        </ProSidebar>
     )
 
 }
